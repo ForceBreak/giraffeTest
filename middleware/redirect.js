9ac52a012f21df87c8455f4cfe4606e07344300b
ForceBreak <gbuecjd18@gmail.com>
2019-03-22 12:27:12 +0200

    edit page redirect

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -2,4 +2,15 @@ export default function ({ route, redirect, store }) {
   if (route.name === 'logout' || route.name === 'delete-id') {
     redirect('/')
   }
+  if (route.name === 'edit-id') {
+    let currentIndex
+    store.state.user.ads.forEach((element, index) => {
+      if (Number(element.id) === Number(route.params.id)) {
+        currentIndex = index
+      }
+    })
+    if (store.state.user.ads[currentIndex].author !== store.state.user.cookieId) {
+      redirect('/')
+    }
+  }
 }
